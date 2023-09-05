5b3ff44d6938d2f6cd91e76555babeeec61d4a8e
rahul2566 <102623925+rahul2566@users.noreply.github.com>
2023-09-05 21:29:41 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
-api_id = 21609776
-api_hash = "61f2558d634df353a2a9806661f4b091"
+api_id = 26600589
+api_hash = "1e06c253a61c1f4f548cbb9ebc09644d"
 bot_token = "5940119184:AAHKEw7V7zGVZvwV4T4RCgmZKfR7lSGKNNE"
-auth_users = [5623849580,-1001861953997]
-sudo_user = 5623849580
-log_channel = -1001791480211
-txt_channel = -1001791480211
+auth_users = [6134859761,-1001757538109]
+sudo_user = 6134859761
+log_channel = -1001852610640
+txt_channel = -1001852610640
